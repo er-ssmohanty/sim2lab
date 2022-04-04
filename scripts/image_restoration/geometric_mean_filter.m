78ff72029c43c5372939194216d066056283b88f
er-ssmohanty <pie2001@protonmail.com>
2022-04-04 23:56:03 +0530

    added mean filters

---
+++ scripts/image_restoration/geometric_mean_filter.m
@@ -0,0 +1,36 @@
+clc;
+close all;
+clear all;
+ 
+rgbi = imread('source_images/putin-stock-free.jpg');
+gri = rgb2gray(rgbi);
+
+[x_ y_] = size(gri);
+
+wrapper_im = rand(x_+2,y_+2);
+wrapper_im(2:x_+1,2:y_+1)=gri;
+
+subim_vect = rand(1,9);
+
+for i = 1:x_
+  for j = 1:y_
+    subim_vect(1:3) = [wrapper_im(i,j),wrapper_im(i,j+1),wrapper_im(i,j+2)];
+    subim_vect(4:6) = [wrapper_im(i+1,j),wrapper_im(i+1,j+1),wrapper_im(i+1,j+2)];
+    subim_vect(7:9) = [wrapper_im(i+2,j),wrapper_im(i+2,j+1),wrapper_im(i+2,j+2)];
+    wrapper_im(i+1,j+1) = mean(subim_vect,"g");  
+  end
+end
+
+
+gri_final = wrapper_im(2:x_+1,2:y_+1);
+gri_final = uint8(gri_final);
+
+subplot(1,2,1);
+imshow(gri_final);
+title('Denoised Image using GMF');%d_not
+
+subplot(1,2,2);
+imshow(gri);
+title('Original Gray Image');
+
+saveas(1,"result_images/generic_gm_filter_0.png");
