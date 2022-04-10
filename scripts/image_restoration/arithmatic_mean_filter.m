c4e190954f052823e3da0f9a7a093221823e3e94
er-ssmohanty <pie2001@protonmail.com>
2022-04-10 12:14:44 +0530

    all ImRestoring programs modularized internally

--- scripts/image_restoration/arithmatic_mean_filter.m
+++ scripts/image_restoration/arithmatic_mean_filter.m
@@ -1,41 +1,69 @@
-clc;
-close all;
-clear all;
- 
-rgbi = imread('source_images/a_woman.jpg');
-gri = rgb2gray(rgbi);
-
-[x_ y_] = size(gri);
-
-wrapper_im = rand(x_+2,y_+2);
-wrapper_im(2:x_+1,2:y_+1)=gri;
-
-%edge
-wrapper_im(1,:)=wrapper_im(2,:);
-wrapper_im(x_+2,:)=wrapper_im(x_+1,:);
-wrapper_im(:,1)=wrapper_im(:,2);
-wrapper_im(:,y_+2)=wrapper_im(:,y_+1);
-
-subim_vect = rand(1,9);
-
-for i = 1:x_
-  for j = 1:y_
-    subim_vect(1:3) = [wrapper_im(i,j),wrapper_im(i,j+1),wrapper_im(i,j+2)];
-    subim_vect(4:6) = [wrapper_im(i+1,j),wrapper_im(i+1,j+1), wrapper_im(i+1,j+2)];
-    subim_vect(7:9) = [wrapper_im(i+2,j),wrapper_im(i+2,j+1),wrapper_im(i+2,j+2)];
-    wrapper_im(i+1,j+1) = mean(subim_vect,"a");  
-  end
+################################################################################
+#
+################################################################################ 
+function wrapper_im = imwrap(a_gray_image)
+	[x_ y_] = size(a_gray_image);
+	wrapper_im = rand(x_+2,y_+2);
+	wrapper_im(2:x_+1,2:y_+1)=a_gray_image;
+	wrapper_im(1,:)=wrapper_im(2,:);
+	wrapper_im(x_+2,:)=wrapper_im(x_+1,:);
+	wrapper_im(:,1)=wrapper_im(:,2);
+	wrapper_im(:,y_+2)=wrapper_im(:,y_+1);
+	#return wrapper_im;
 end
+################################################################################
+#
+################################################################################
+function image_filtered = filter_any(image_,m_,n_)
+	image_filtered = image_;
+	subim_vect = rand(1,(m_*n_)-1);
+	for i = 1:x_
+		for j = 1:y_
+			subim_vect(1:3) = [image_(i,j),image_(i,j+1),image_(i,j+2)];
+			subim_vect(4:6) = [image_(i+1,j),image_(i+1,j+1),image_(i+1,j+2)];
+			subim_vect(7:9) = [image_(i+2,j),image_(i+2,j+1),image_(i+2,j+2)];
+			image_filtered(i+1,j+1) = mean(subim_vect,"a");
+		end
+	end
+	#return image_filtered;
+end
+################################################################################
+#
+################################################################################
+function result_im = main(Image_full_path,sub_image_m=3,sub_image_n=3)
+	rgbi = imread(Image_full_path);
+	gri = rgb2gray(rgbi);
+	wrapped = imwrap(gri);
+	filtered = filter_any(wrapped,sub_image_m,sub_image_n);
+	unwrapped_filtered = filtered(2:x_+1,2:y_+1);
+	result_im = uint8(unwrapped_filtered);
+end
+################################################################################
+#
+################################################################################
+function clean_()
+	clc;
+	close all;
+	clear all;
+end
+################################################################################
+################################################################################
 
-gri_final = wrapper_im(2:x_+1,2:y_+1);
-gri_final = uint8(gri_final);
+clean_();
+image_path = 'source_images/spices0.jpg';
+gri_final = main(image_path);
+gri = rgb2gray(imread(image_path));
 
+
+
+###################################################
 subplot(1,2,1);
 imshow(gri_final);
-title('Denoised Image using AMF');%d_not
+title('Denoised Image using Arithmatic Mean Filter');#d_not
 
 subplot(1,2,2);
 imshow(gri);
 title('Original Gray Image');
 
-saveas(1,"result_images/generic_am_filter_0.png");
+saveas(1,"result_images/generic_am_filter_1.png");
+#####################################################
--- scripts/image_restoration/median_filter.m
+++ scripts/image_restoration/median_filter.m
@@ -4,7 +4,7 @@
 function wrapper_im = imwrap(a_gray_image)
 	[x_ y_] = size(a_gray_image);
 	wrapper_im = rand(x_+2,y_+2);
-	wrapper_im(2:x_+1,2:y_+1)=gri;
+	wrapper_im(2:x_+1,2:y_+1)=a_gray_image;
 	wrapper_im(1,:)=wrapper_im(2,:);
 	wrapper_im(x_+2,:)=wrapper_im(x_+1,:);
 	wrapper_im(:,1)=wrapper_im(:,2);
@@ -14,17 +14,18 @@ end
 ################################################################################
 #
 ################################################################################
-function image_ = filter_any(image_,m_,n_)
+function image_filtered = filter_any(image_,m_,n_)
+	image_filtered = image_;
 	subim_vect = rand(1,(m_*n_)-1);
 	for i = 1:x_
 		for j = 1:y_
 			subim_vect(1:3) = [image_(i,j),image_(i,j+1),image_(i,j+2)];
 			subim_vect(4:6) = [image_(i+1,j),image_(i+1,j+1),image_(i+1,j+2)];
 			subim_vect(7:9) = [image_(i+2,j),image_(i+2,j+1),image_(i+2,j+2)];
-			image_(i+1,j+1) = median(subim_vect);
+			image_filtered(i+1,j+1) = median(subim_vect);
 		end
 	end
-	#return image_;
+	#return image_filtered;
 end
 ################################################################################
 #
@@ -54,6 +55,8 @@ gri_final = main(image_path);
 gri = rgb2gray(imread(image_path));
 
 
+
+###################################################
 subplot(1,2,1);
 imshow(gri_final);
 title('Denoised Image using Median Filter');#d_not
@@ -62,4 +65,5 @@ subplot(1,2,2);
 imshow(gri);
 title('Original Gray Image');
 
-saveas(1,"result_images/median_filter_0.png");
+saveas(1,"result_images/generic_median_filter_1.png");
+#####################################################
